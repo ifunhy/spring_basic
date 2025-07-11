a6caef80578fd5d3f6ec9a876957d339956e7bd0
ifunhy <guswl8296@gmail.com>
2025-07-11 10:19:33 +0900

    case6~

--- src/main/java/com/beyond/basic/b1_hello/controller/Hello.java
+++ src/main/java/com/beyond/basic/b1_hello/controller/Hello.java
@@ -1,24 +1,13 @@
 package com.beyond.basic.b1_hello.controller;
 
+import lombok.*;
+
+//@Getter // 클래스 내의 모든 변수를 대상으로 getter 생성
+@Data   // getter, setter, toString 메소드까지 모두 만들어주는 어노테이션
+@AllArgsConstructor // 모든 매개변수가 있는 생성자
+@NoArgsConstructor  // 기본생성자
+// 기본생성자 + getter의 조합은 parsing이 이뤄지므로 보통은 필수적 요소
 public class Hello {
     private String name;
     private String email;
-
-    public Hello(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    @Override
-    public String toString() {
-        return "Hello{" + "name='" + name + '\'' + ", email='" + email + '\'' + '}';
-    }
 }
