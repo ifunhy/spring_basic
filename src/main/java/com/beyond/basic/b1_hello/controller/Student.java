71e35b14a3a28040d6fdf60d00dc60a61e7f0ab4
ifunhy <guswl8296@gmail.com>
2025-07-11 17:48:29 +0900

    0711

--- src/main/java/com/beyond/basic/b1_hello/controller/Student.java
+++ src/main/java/com/beyond/basic/b1_hello/controller/Student.java
@@ -2,14 +2,23 @@ package com.beyond.basic.b1_hello.controller;
 
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
+import java.util.List;
 
-//@Getter // 클래스 내의 모든 변수를 대상으로 getter 생성
 @Data   // getter, setter, toString 메소드까지 모두 만들어주는 어노테이션
 @AllArgsConstructor // 모든 매개변수가 있는 생성자
 @NoArgsConstructor  // 기본생성자
 // 기본생성자 + getter의 조합은 parsing이 이뤄지므로 보통은 필수적 요소
-public class Hello {
+public class Student {
     private String name;
     private String email;
-//    private MultipartFile photo;
+    private List<Score> scores;
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Data
+    private static class Score{
+        private String subject;
+        private int point;
+    }
 }
+// {name:"test", email:"test@naver", scores:[{subject:"수학", point:80}, {subject:"영어", point:90}]};
